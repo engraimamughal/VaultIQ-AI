6abf1f6e2599225b5722f7f88e7bbf7a4a2f141f
Aima <itxaimamughal5@gmail.com>
2026-09-01 16:40:19 +0500

    Move Firebase API key to environment variable

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWxq29a8zbxoHSjmbtEM-xJppThojL3qs",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "vaultiq-ai.firebaseapp.com",
   projectId: "vaultiq-ai",
   storageBucket: "vaultiq-ai.firebasestorage.app",
   messagingSenderId: "444800321203",
-  appId: "1:444800321203:web:a11d1a62ea33c3138b92c6"
+  appId: "1:444800321203:web:a11d1a62ea33c3138b92c6",
 };
-
 
 const app = initializeApp(firebaseConfig);
 
